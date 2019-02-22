428c66fa16faddbc7040b23d21fa13ad01703544
guanglei1015 <3268188701@qq.com>
2019-02-22 15:17:27 +0800

    新建 分支 git_branch

--- dome02/src/main/java/com/wkcto/Demo2.java
+++ dome02/src/main/java/com/wkcto/Demo2.java
@@ -2,7 +2,9 @@ package com.wkcto;
 
 public class Demo2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Demo2====");
+        System.out.println("分支======");
     }
 }
